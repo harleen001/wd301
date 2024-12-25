225c51ea1acb00ca19edd7b3267a68120eba3b10
harleen <harleen.johal31@gmail.com>
2024-12-25 13:32:44 +0530

    props-understood

--- hello_react/src/App.jsx
+++ hello_react/src/App.jsx
@@ -1,14 +1,28 @@
 
-import './App.css'
+import TaskCard from "./TaskCard";
 
 function App() {
   
-let name = "Harleen"
-const numbers = [1,2,4,5]
+{/* let name = "Harleen"
+const numbers = [1,2,4,5]  */ }
   return (
    <>
-  <h1>Hello {name}</h1>
-  <h2>these are numbers {numbers}</h2>
+ {/* <h1>Hello {name}</h1>
+  <h2>these are numbers {numbers}</h2> */}
+<div>
+  <div> <h1>Pending</h1>
+  <TaskCard title="Title for 1st Card" card="abc" />
+  <TaskCard title="Title for 2nd Card" card="def"/>
+  </div>
+
+  <div> <h1>Done</h1>
+  <TaskCard title="Title for 3rd Card" card="ghi" />
+  <TaskCard title="Title for 4th Card" card="jkl" />
+  </div>
+
+
+</div>
+
   </>
   );
 }
---
+++ hello_react/src/TaskCard.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+const TaskCard = (props) =>{
+  console.log(props);
+
+    return(
+      <div>
+      <h2>{props.title}</h2>
+      <p>{props.card}</p>
+      <p>Assignee: Harleen Singh</p>
+    </div>
+  
+    )
+  }
+  export default TaskCard;
