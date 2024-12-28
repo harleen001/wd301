a311b15594e69cf912ff14d8e7d64ff476b94240
harleen <harleen.johal31@gmail.com>
2024-12-28 20:47:45 +0530

    typescript-console-habits

--- hello_react/src/index.ts
+++ hello_react/src/index.ts
@@ -18,4 +18,35 @@ let User2: User = {
 
 console.log("Harleen is a Valid Voter",User1.isValidVoter);
 console.log("Imanpal is a Valid Voter",User2.isValidVoter);
+
+
+let MYNUMBER : number;
+//MYNUMBER="HELLO WORLD"; //ERROR
+MYNUMBER = 5;
+console.log(MYNUMBER);
 //tsx .\src\index.ts  WILL RUN THIS THING
+
+//ANY DATA TYPE
+let names: any = "hello";
+names = 42;
+names = false;
+//void function
+function printHello(): void {
+    console.log("Hello!");
+  }
+
+  //throw error
+  function throwError(): never {
+    throw new Error("An error occurred!");
+  }
+
+  //taking annotations from string or like a parameter
+  function addUser(user: User): string {
+    return user.name + " added successfully";
+}
+console.log(addUser(User1));
+
+let userName = "Jane";  // type: string
+let userID = 10;  // type: number
+let uniqueID = userName + userID;
+console.log(uniqueID);
