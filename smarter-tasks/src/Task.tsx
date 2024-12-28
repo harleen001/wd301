70b1fb94e1a60817df0698e3058fe3dc4d0efe27
harleen <harleen.johal31@gmail.com>
2024-12-28 23:45:41 +0530

    smarter-tasks

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,4 +1,9 @@
 import React from "react";
 
-class Task extends React.Component {}
+class Task extends React.Component {
+    render() {
+      return <div>Buy groceries </div>;
+    }
+  }
+
 export default Task;
